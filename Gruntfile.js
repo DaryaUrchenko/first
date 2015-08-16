ff8d1bb722352969003313fa10b1153965815ff3
UDarya <dasha_ula@mail.ru>
2015-08-16 21:25:46 +0300

    grunt
    Signed-off-by: UDarya <dasha_ula@mail.ru>

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		// watch: {
+		// 	fest: {
+		// 		files: ['templates/*.xml'],
+		// 		task: ['fest'],
+		// 		options: {
+		// 			interrupt: true,
+		// 			atBegin: true
+		// 		}
+		// 	}
+		// },
+		shell: {
+			options: {
+				stdout: true,
+				stderr: true
+			},
+			server: {
+				command: 'java -cp out/artifacts/first_jar/first.jar main.Main 8080' /* запуск сервера */
+			},
+		fest: {
+			templates: {
+				files: [{
+					expand: true,
+					cwd: 'templates', // исходная директория
+					src: '*xml',
+					dest: 'public_html/js/tmpl'
+				}],
+				options: {
+					templates: function (data) { // задаем формат функции шаблна
+						return grunt.template.process(
+							'var <%= name %>Tmpl = <%= contents %> ;', // присваеваем функцию-шаблон переменной
+							{data: data}
+							);
+					}
+				}
+			}
+		}
+		}
+		// concurrent: {
+		// 	targert: ['watch', 'shell'],
+		// 	options: {
+		// 		logConcurrentOutput: true // output logs
+		// 	}
+		// }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-fest');
+
+	// grunt.registerTask('default', ['concurrent']);
+};
